e9c23b0c47fe1e4f97519467cf3c975ec9b951b8
HabibJR <smhabib.abir2030@gmail.com>
2023-02-12 22:32:54 +0100

    add default status zero

--- db/migrate/20230208215515_create_todos.rb
+++ db/migrate/20230208215515_create_todos.rb
@@ -2,7 +2,7 @@ class CreateTodos < ActiveRecord::Migration[7.0]
   def change
     create_table :todos do |t|
       t.string :title
-      t.integer :status, default: 1
+      t.integer :status, default: 0
 
       t.timestamps
     end
